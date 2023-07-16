be57df8b8a8b1c6d48fd0594f35951daae330a71
thanhnd <ducthanhnguyen95@gmail.com>
2023-07-16 15:44:25 +0700

    How does CORS work?

---
+++ src/main/java/com/example/using/cross/origin/resource/sharing/UsingCrossOriginResourceSharingApplication.java
@@ -0,0 +1,13 @@
+package com.example.using.cross.origin.resource.sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingCrossOriginResourceSharingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsingCrossOriginResourceSharingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/using/cross/origin/resource/sharing/UsingCrossOriginResourceSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.using.cross.origin.resource.sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsingCrossOriginResourceSharingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
